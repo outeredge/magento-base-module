ce57718abd08a9de0678d3da9dbec46f1713f408
Victor Seager <vseager@gmail.com>
2015-02-19 17:00:02 +0000

    fixed extra slash in imageUrl

--- app/code/local/Edge/Base/Helper/Image.php
+++ app/code/local/Edge/Base/Helper/Image.php
@@ -4,12 +4,13 @@ class Edge_Base_Helper_Image extends Mage_Core_Helper_Abstract
 {
     public function getImage($file)
     {
-        if (($file) && (0 !== strpos($file, '/', 0))) {
-            $file = '/' . $file;
-        }
-
         $mediaDir = Mage::getBaseDir('media');
-        $imageDir = $mediaDir . $file;
+        
+        if (($file) && (0 !== strpos($file, '/', 0))) {
+            $mediaDir = $mediaDir . '/';
+        }
+        
+        $imageDir = $mediaDir . $file;        
 
         if (!file_exists($imageDir)){
             // If the file does not exist, create it from the database
