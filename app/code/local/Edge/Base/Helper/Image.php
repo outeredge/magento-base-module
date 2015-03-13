8675f72120c2308c24c4a76bf09dc23f4466ac94
Minlare <mike@outeredgeuk.com>
2015-03-13 11:57:44 +0000

    Modify controls for images

--- app/code/local/Edge/Base/Helper/Image.php
+++ app/code/local/Edge/Base/Helper/Image.php
@@ -2,24 +2,163 @@
 
 class Edge_Base_Helper_Image extends Mage_Core_Helper_Abstract
 {
+    protected $_scheduleModify;
+
+    protected $_width  = null;
+    protected $_height = null;
+    protected $_quality = 100;
+
+    protected $_keepAspectRatio;
+    protected $_keepFrame;
+    protected $_constrainOnly;
+
+    protected $_crop;
+
     public function getImage($file)
     {
         $mediaDir = Mage::getBaseDir('media');
-        
+
         if (($file) && (0 !== strpos($file, '/', 0))) {
             $mediaDir = $mediaDir . '/';
         }
-        
-        $imageDir = $mediaDir . $file;        
+
+        $imageDir = $mediaDir . $file;
 
         if (!file_exists($imageDir)){
             // If the file does not exist, create it from the database
             Mage::helper('core/file_storage_database')->saveFileToFilesystem($imageDir);
         }
 
+        if ($this->_scheduleModify) {
+            return $this->_getModifiedImage($file);
+        }
+
         $mediaUrl = Mage::getBaseUrl('media');
         $imageUrl = $mediaUrl . $file;
 
         return $imageUrl;
+    }
+
+    protected function _getModifiedImage($file)
+    {
+        $mediaDir = Mage::getBaseDir('media') . DS;
+        $filePath = $mediaDir . $file;
+
+        if ($this->_width || $this->_height) {
+            if (!$this->_width) {
+                $this->_width = $this->_height;
+            }
+            if (!$this->_height) {
+                $this->_height = $this->_width;
+            }
+        }
+
+        $mediaUrl = Mage::getBaseUrl('media');
+
+        $fileParts = explode('/', $file);
+        $fileName = array_pop($fileParts);
+        $fileParts[] = $this->_width . 'x' . $this->_height;
+        if ($this->_crop) {
+            $fileParts[] = 'crop';
+        }
+        $fileParts[] = '';
+        $modifiedPath = implode(DS, $fileParts);
+        $modifiedUrl = $mediaDir . $modifiedPath . $fileName;
+
+        if (!file_exists($modifiedUrl)) {
+
+            $image = new Varien_Image($filePath);
+            $image->quality($this->_quality);
+
+            if ($this->_keepAspectRatio) {
+                $image->keepAspectRatio($this->_keepAspectRatio);
+            }
+            if ($this->_keepFrame) {
+                $image->keepFrame($this->_keepFrame);
+            }
+            if ($this->_constrainOnly) {
+                $image->constrainOnly($this->_constrainOnly);
+            }
+
+            $originalAspectRatio = $image->getOriginalWidth() / $image->getOriginalHeight();
+            $aspectRatio = $this->_width / $this->_height;
+
+            if ($this->_crop) {
+                if ($aspectRatio < $originalAspectRatio) {
+                    // Narrower than original
+                    $newWidth = ceil($this->_height * $originalAspectRatio);
+                    $cropHorizontal = ($newWidth - $this->_width) / 2;
+                    $image->resize($newWidth, $this->_height);
+                    $image->crop(0, $cropHorizontal, $cropHorizontal, 0);
+                }
+                else {
+                    // Shorter than original
+                    $newHeight = ceil($this->_width / $originalAspectRatio);
+                    $cropVertical = ($newHeight - $this->_height) / 2;
+                    $image->resize($this->_width, $newHeight);
+                    $image->crop($cropVertical, 0, 0, $cropVertical);
+                }
+            }
+            else {
+                $image->resize($this->_width, $this->_height);
+            }
+
+            $modifiedUrl = $mediaDir . $modifiedPath . $fileName;
+            $image->save($modifiedUrl);
+        }
+
+        return $mediaUrl . $modifiedPath . $fileName;
+    }
+
+    public function setSize($width, $height=null)
+    {
+        $this->_width = $width;
+        $this->_height = $height;
+        $this->_scheduleModify = true;
+        return $this;
+    }
+
+    public function setWidth($width)
+    {
+        $this->_width = $width;
+        $this->_scheduleModify = true;
+        return $this;
+    }
+
+    public function setHeight($height)
+    {
+        $this->_height = $height;
+        $this->_scheduleModify = true;
+        return $this;
+    }
+
+    public function setQuality($quality)
+    {
+        $this->_quality = $quality;
+        return $this;
+    }
+
+    public function setKeepAspectRatio($keepAspectRatio)
+    {
+        $this->_keepAspectRatio = $keepAspectRatio;
+        return $this;
+    }
+
+    public function setKeepFrame($keepFrame)
+    {
+        $this->_keepFrame = $keepFrame;
+        return $this;
+    }
+
+    public function setConstrainOnly($constrainOnly)
+    {
+        $this->_constrainOnly = $constrainOnly;
+        return $this;
+    }
+
+    public function setCrop($crop)
+    {
+        $this->_crop = $crop;
+        return $this;
     }
 }
