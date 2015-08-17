97ae086da0e0ac37ce8b663c1aa09082f29f858b
Michael Windell <mike@outeredgeuk.com>
2015-08-17 11:56:22 +0100

    Quick Fix - Update directory_id on Separate Media Database

--- app/code/local/Edge/Base/Model/Core/File/Storage/Database.php
+++ app/code/local/Edge/Base/Model/Core/File/Storage/Database.php
@@ -3,6 +3,42 @@
 class Edge_Base_Model_Core_File_Storage_Database extends Mage_Core_Model_File_Storage_Database
 {
     /**
+     * Rename files in database
+     *
+     * @param  string $oldFilePath
+     * @param  string $newFilePath
+     * @return Mage_Core_Model_File_Storage_Database
+     */
+    public function renameFile($oldFilePath, $newFilePath)
+    {
+        $this->_getResource()->renameFile(
+            basename($oldFilePath),
+            dirname($oldFilePath),
+            basename($newFilePath),
+            dirname($newFilePath)
+        );
+
+        $newPath = dirname($newFilePath);
+        $directory = Mage::getModel('core/file_storage_directory_database')->loadByPath($newPath);
+
+        if (!$directory->getId()) {
+            $directory = $this->getDirectoryModel()->createRecursive($newPath);
+        }
+
+        $this->loadByFilename($newFilePath);
+        if ($this->getId()) {
+            $this->setDirectoryId($directory->getId())->save();
+
+            // outer/edge Quick Fix
+            // Separate Database connection stops the directory_id being updated
+            $db = Mage::getSingleton('core/resource')->getConnection($this->getConfigConnectionName());
+            $db->update('core_file_storage', array('directory_id' => $directory->getId()), "file_id = {$this->getId()}");
+        }
+
+        return $this;
+    }
+
+    /**
      * Return directory listing
      *
      * @param string $directory
