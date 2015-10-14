dd8a557532d46834ee8391f483cd208176d6eb4a
Michael Windell <mike@outeredgeuk.com>
2015-10-14 15:57:51 +0100

    Avoid Using LIKE on deleteFolder()

--- app/code/local/Edge/Base/Model/Resource/Core/File/Storage/Database.php
+++ app/code/local/Edge/Base/Model/Resource/Core/File/Storage/Database.php
@@ -26,5 +26,35 @@ class Edge_Base_Model_Resource_Core_File_Storage_Database extends Mage_Core_Mode
 
         return $adapter->fetchAll($select);
     }
+
+    /**
+     * Delete files that starts with given $folderName
+     * Avoid using LIKE% on delete
+     *
+     * @param string $folderName
+     */
+    public function deleteFolder($folderName = '')
+    {
+        $folderName = rtrim($folderName, '/');
+        if (!strlen($folderName)) {
+            return;
+        }
+
+        /* @var $resHelper Mage_Core_Model_Resource_Helper_Abstract */
+        $resHelper = Mage::getResourceHelper('core');
+        $likeExpression = $resHelper->addLikeEscape($folderName . '/', array('position' => 'start'));
+
+        $fileIds = $this->_getReadAdapter()->fetchCol(
+            $this->_getReadAdapter()
+                ->select()
+                ->from($this->getMainTable(), array($this->getIdFieldName()))
+                ->where(new Zend_Db_Expr('directory LIKE ' . $likeExpression))
+            );
+
+        if (!empty($fileIds)) {
+            $this->_getWriteAdapter()
+                ->delete($this->getMainTable(), $this->_getWriteAdapter()->quoteInto($this->getIdFieldName() . ' IN (?)', $fileIds));
+        }
+    }
 }
 
