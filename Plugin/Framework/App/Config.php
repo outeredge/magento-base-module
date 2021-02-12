4f5543254158dd52d5d595c3ac7c0193f76aab71
David Windell <david@outeredgeuk.com>
2021-02-12 16:05:59 +0000

    Disable CDN's in Admin

---
+++ Plugin/Framework/App/Config.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace OuterEdge\Base\Plugin\Framework\App;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\Config as AppConfig;
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Store\Model\Store;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Config
+{
+    /**
+     * @var State
+     */
+    protected $state;
+
+    /**
+     * @param State $state
+     */
+    public function __construct(State $state)
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * Disable CDN's in Admin
+     */
+    public function aroundGetValue(AppConfig $subject, callable $proceed,
+        $path = null,
+        $scope = ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
+        $scopeCode = null
+    ) {
+        if ($path == Store::XML_PATH_SECURE_BASE_MEDIA_URL || 
+            $path == Store::XML_PATH_UNSECURE_BASE_MEDIA_URL
+        ) {
+            try {
+                if($this->state->getAreaCode() == Area::AREA_ADMINHTML) {
+                    return null;
+                }
+            } catch (LocalizedException $ex) {
+                // ignore
+            }
+        }
+
+        return $proceed($path, $scope, $scopeCode);
+    }
+}
