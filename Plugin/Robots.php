1c0c815525b9bfa86feeb475eccf021228fcc452
Bruno Dias <ibroopi@gmail.com>
2022-10-11 12:00:00 +0100

    Changed robots metadata if url have specific parameteres

---
+++ Plugin/Robots.php
@@ -0,0 +1,63 @@
+<?php
+namespace OuterEdge\Base\Plugin;
+
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\View\Page\Config as PageConfig;
+use Magento\Framework\View\Page\Config\Renderer;
+
+class Robots
+{
+    protected $queryStrings = [
+        'dir', 
+        'limit', 
+        'order', 
+        '___from_store', 
+        '__store'
+    ];
+
+    /**
+     * @var PageConfig
+     */
+    protected $pageConfig;
+
+    /**
+     * @var Http
+     */
+    protected $request;
+
+    /**
+     *
+     * @param PageConfig $pageConfig
+     * @param Http $request
+     */
+    public function __construct(
+        PageConfig $pageConfig,
+        Http $request
+    ) {
+        $this->pageConfig = $pageConfig;
+        $this->request = $request;
+    }
+
+    /**
+     * Before Render function
+     *
+     * @param Renderer $subject
+     */
+    public function beforeRenderMetadata(Renderer $subject)
+    {
+        $params = $this->request->getParams();
+
+        $control = false;
+        if ($params) {
+            foreach ($params as $key => $value) {
+                if (in_array($key, $this->queryStrings)) {
+                    $control = true;
+                }
+            }
+        }
+
+        if ($control) {
+            $this->pageConfig->setMetadata('robots', 'NOINDEX,NOFOLLOW');
+        }
+    }
+}
