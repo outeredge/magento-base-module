1c63c44f481ab3b7f81892430206aea1d5d0862b
Bruno Dias <ibroopi@gmail.com>
2023-01-19 17:10:10 +0000

    Created logic for config changed command

--- Console/Command/ConfigChanged.php
+++ Console/Command/ConfigChanged.php
@@ -9,20 +9,39 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Magento\Framework\App\ResourceConnection;
 
 class ConfigChanged extends Command
 {
-    private const NAME = 'name';
+    private const HOURS = 'hours';
+
+    private const LINES = 'lines';
+
+    protected $resourceConnection;
+
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        string $name = null
+    ) {
+        parent::__construct($name);
+        $this->resourceConnection = $resourceConnection;
+    }
 
     protected function configure(): void
     {
         $this->setName('config:changed:command');
-        $this->setDescription('This is my first console command.');
+        $this->setDescription('List recent core_config_data changes');
         $this->addOption(
-            self::NAME,
+            self::HOURS,
             null,
             InputOption::VALUE_REQUIRED,
-            'Name'
+            'Hours'
+        );
+        $this->addOption(
+            self::LINES,
+            null,
+            InputOption::VALUE_REQUIRED,
+            'Lines'
         );
 
         parent::configure();
@@ -36,29 +55,104 @@ class ConfigChanged extends Command
      *
      * @return int
      */
-     protected function execute(InputInterface $input, OutputInterface $output): int
-     {
-         $exitCode = 0;
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $exitCode = 0;
+        $existInEnvFile = false;
+        $differentFromEnvFile = false;
 
-         if ($name = $input->getOption(self::NAME)) {
-             $output->writeln('<info>Provided name is `' . $name . '`</info>');
-         }
+        if ($hours = $input->getOption(self::HOURS)) {
+            $output->writeln('<info>Provided hours is `' . $hours . '`</info>');
+        } else {
+            $hours = 240;
+            $output->writeln('<info>Default hours is `' . $hours . '`</info>');
+        }
 
-         $output->writeln('<info>Success message.</info>');
-         $output->writeln('<comment>Some comment.</comment>');
+        if ($lines = $input->getOption(self::LINES)) {
+            $output->writeln('<info>Provided lines is `' . $lines . '`</info>');
+        } else {
+            $lines = 25;
+            $output->writeln('<info>Default lines is `' . $lines . '`</info>');
+        }
 
-         try {
-             if (rand(0, 1)) {
-                throw new LocalizedException(__('An error occurred.'));
-             }
-         } catch (LocalizedException $e) {
-             $output->writeln(sprintf(
-                 '<error>%s</error>',
-                 $e->getMessage()
-             ));
-             $exitCode = 1;
-         }
+        try {
+            //Read core config data from db
+            $connection = $this->resourceConnection->getConnection();
+            $table = $connection->getTableName('core_config_data');
+            //ToDo add hours verification
+            $query = "SELECT * FROM $table ORDER BY updated_at DESC LIMIT $lines";
+            $dbConfigData = $connection->fetchAll($query);
 
-         return $exitCode;
-     }
+            //Read env.php file
+            $files = glob(__DIR__ . '/../../../../../app/etc/env.php.*');
+            if (empty($files) || empty($_SERVER['CONFIG__DEFAULT__WEB__SECURE__BASE_URL'])) {
+                throw new LocalizedException(__('Config files not found'));
+            }
+
+            //ToDo check if I need this, on is only env.php file
+            $validStores = ['default' => 'default*'];
+            foreach ($files as $fileConfig) {
+
+                $code = substr(strrchr($fileConfig, '.'), 1);
+                if ($code != 'production') {
+                    $validStores[$code] = $code;
+                }
+            }
+
+            //Get env and config togetther
+            $fileConfig = glob(__DIR__ . '/../../../../../app/etc/config.php');
+            $fileConfig = $fileConfig[0];
+            $fileConfigDataArray = include($fileConfig);
+            $fileConfigData = $this->flatten($fileConfigDataArray['system']['default']);
+
+            $output->writeln('<info>Recent core_config_data changes.</info>');
+            foreach ($dbConfigData as $dbConfig) {
+
+                $dbPatch = $dbConfig['path'];
+
+                if (array_key_exists($dbPatch, $fileConfigData)) {
+                    $existInEnvFile = true;
+                    $fileConfigSelectedValue = $fileConfigData[$dbPatch];
+                    $dbConfigValue = $dbConfig['value'];
+
+                    if ($fileConfigSelectedValue != $dbConfigValue) {
+                        $differentFromEnvFile = true;
+                    } else {
+                        $differentFromEnvFile = false;
+                    }
+                } else {
+                    $existInEnvFile = false;
+                }
+
+                if ($existInEnvFile && $differentFromEnvFile) {
+                    $output->writeln('<error>Need file update: '. $dbPatch . ' :: ' . $dbConfig['value']. '</error>');
+                }
+
+                $output->writeln('<info>'.$dbPatch . ' :: ' . $dbConfig['value'].'</info>');
+            }
+
+            $output->writeln('<info>Completed.</info>');
+        } catch (LocalizedException $e) {
+            $output->writeln(sprintf(
+                '<error>%s</error>',
+                $e->getMessage()
+            ));
+            $exitCode = 1;
+        }
+
+        return $exitCode;
+    }
+
+    private function flatten($array, $prefix = '') {
+        $result = array();
+        foreach($array as $key=>$value) {
+            if(is_array($value)) {
+                $result = $result + $this->flatten($value, $prefix . $key . '/');
+            }
+            else {
+                $result[$prefix . $key] = $value;
+            }
+        }
+        return $result;
+    }
 }
