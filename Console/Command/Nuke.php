60d354375f6a495db569707d58791811671a0643
Bruno Dias <ibroopi@gmail.com>
2023-11-17 11:36:23 +0000

    Improved nuke command and add acl

--- Console/Command/Nuke.php
+++ Console/Command/Nuke.php
@@ -6,14 +6,27 @@ namespace OuterEdge\Base\Console\Command;
 
 use Magento\Framework\Exception\LocalizedException;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\App\State\CleanupFiles;
+use Magento\Framework\App\Cache\Frontend\Pool;
 
 class Nuke extends Command
 {
+    protected $cleanupFiles;
+
+    protected $cacheFrontendPool;
+
+    public function __construct(
+        CleanupFiles $cleanupFiles,
+        Pool $cacheFrontendPool,
+        string $name = null
+    ) {
+        parent::__construct($name);
+        $this->cleanupFiles = $cleanupFiles;
+        $this->cacheFrontendPool = $cacheFrontendPool;
+    }
+
     protected function configure(): void
     {
         $this->setName('outeredge:nuke');
@@ -32,11 +45,25 @@ class Nuke extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        if (shell_exec('rm -rf generated pub/static/* var/view_preprocessed && bin/magento c:f')) {
+        $exitCode = 0;
+
+        try {
+            $this->cleanupFiles->clearCodeGeneratedClasses();
+            $this->cleanupFiles->clearAllFiles();
+
+            foreach ($this->cacheFrontendPool as $cacheFrontend) {
+                $cacheFrontend->getBackend()->clean();
+            }
+
             $output->writeln('<comment>☢️ Successfully nuked all Magento cache, static content and generated files ☢️</comment>');
-            return 0;
+        } catch (LocalizedException $e) {
+            $output->writeln(sprintf(
+                '<error>%s</error>',
+                $e->getMessage()
+            ));
+            $exitCode = 1;
         }
 
-        return 1;
+        return $exitCode;
     }
 }
