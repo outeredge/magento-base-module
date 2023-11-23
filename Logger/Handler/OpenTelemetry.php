cc9ea210256d2d69199066d8f61e4e7bc8040e35
Bruno Dias <ibroopi@gmail.com>
2023-11-23 12:42:12 +0000

    Step by step

--- Logger/Handler/OpenTelemetry.php
+++ Logger/Handler/OpenTelemetry.php
@@ -6,6 +6,12 @@ use Exception;
 use Magento\Framework\Filesystem\DriverInterface;
 use Magento\Framework\Logger\Handler\Exception as ExceptionHandler;
 use Magento\Framework\Logger\Handler\Base;
+use OpenTelemetry\SDK\Logs\LoggerProvider;
+use Monolog\Level;
+use Monolog\Logger;
+use OpenTelemetry\Contrib\Logs\Monolog\Handler;
+use Psr\Log\LogLevel;
+use OpenTelemetry\API\Globals;
 
 class OpenTelemetry extends Base
 {
@@ -37,12 +43,50 @@ class OpenTelemetry extends Base
      */
     public function write(array $record): void
     {
+        putenv('OTEL_PHP_AUTOLOAD_ENABLED=true');
+        putenv('OTEL_METRICS_EXPORTER=none');
+        putenv('OTEL_LOGS_EXPORTER=otlp');
+        putenv('OTEL_LOGS_PROCESSOR=batch');
+        putenv('OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf');
+        putenv('OTEL_EXPORTER_OTLP_ENDPOINT=https://otlp.eu01.nr-data.net:4318');
+        putenv('OTEL_EXPORTER_OTLP_CLIENT_KEY=NRAK-7ZX2SPADU1KU6LNYYDYP0ZZWKOK');
+        putenv('OTEL_EXPORTER_OTLP_CLIENT_KEY_ID=1EA8E3120801608EC01E11BBDD7A0821D77A35E315B1206A7CB57B1DD29D3199');
+
+        $handler = new Handler(
+            Globals::loggerProvider(),
+            LogLevel::INFO,
+            true,
+        );
+        $logger = new Logger(
+            'example',
+            [$handler],
+        );
+
+        $logger->info('Bruno Test');
+
+        /*$handler = new Handler(
+            OpenTelemetry\API\Globals::loggerProvider(),
+            LogLevel::INFO, //or `Logger::INFO`, or `Level::Info` depending on monolog version
+            true,
+        );
+        $logger = new Logger(
+            'example',
+            [$handler],
+        );
+
+        $logger->info('hello, otel');
+        $logger->error('something went wrong', [
+            'foo' => 'bar',
+            'exception' => new Exception('something went wrong', 500, new Exception('the first exception', 99)),
+        ]);*/
+
+
 
         //print_r($record); die();
 
         if (isset($record['context']['exception'])) {
 
-        $loggerProvider = new \OpenTelemetry\SDK\Logs\LoggerProvider($record['context']['exception']);
+        $loggerProvider = new OpenTelemetry\SDK\Logs\LoggerProvider($record['context']['exception']);
             $handler = new \OpenTelemetry\Contrib\Logs\Monolog\Handler(
                 $loggerProvider,
                 'info',
