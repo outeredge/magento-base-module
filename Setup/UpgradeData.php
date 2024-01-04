c40b24d00a7d5ec9bc2745770efa82e8f5cd256e
Bruno Dias <ibroopi@gmail.com>
2024-01-04 16:22:00 +0000

    Upgrade Data file

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OuterEdge\Base\Setup;
+
+use Magento\Integration\Model\ConfigBasedIntegrationManager;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    private $integrationManager;
+
+    public function __construct(ConfigBasedIntegrationManager $integrationManager)
+    {
+        $this->integrationManager = $integrationManager;
+    }
+    
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->integrationManager->processIntegrationConfig(['remoteSiteStatus']);
+    }
+}
